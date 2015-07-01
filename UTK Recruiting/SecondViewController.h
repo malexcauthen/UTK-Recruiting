9ad89fbdd9e43704949a7b4957bded57c41dcc15
malexcauthen <mcauthen@vols.utk.edu>
2015-07-01 16:25:26 -0400

    Initial Commit

---
+++ UTK Recruiting/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  UTK Recruiting
+//
+//  Created by Alex Cauthen on 7/1/15.
+//  Copyright (c) 2015 malexcauthen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
